0681be2a93b5daa7eb86f97716c61fd8995fea9c
jacob-huss <72151699+jacob-huss@users.noreply.github.com>
2020-10-27 08:51:26 -0500

    Added getters setters role

---
+++ src/main/java/com/tts/techtalenttwitter/model/User.java
@@ -0,0 +1,42 @@
+package com.tts.techtalenttwitter.model;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="user_Id")
+    private Long id;
+
+    private String email;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int active;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+    
+}
