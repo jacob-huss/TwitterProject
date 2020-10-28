85aac650e981ec5874d8b2ca0c195cc07a286c4f
jacob-huss <72151699+jacob-huss@users.noreply.github.com>
2020-10-27 23:34:41 -0500

    TwitterProject One

--- src/main/java/com/tts/techtalenttwitter/model/Role.java
+++ src/main/java/com/tts/techtalenttwitter/model/Role.java
@@ -1,50 +1,32 @@
 package com.tts.techtalenttwitter.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import javax.persistence.*;
 
 
 @Entity
 public class Role {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "role_id")
     private Long id;
     private String role;
 
-    public Role(Long id, String role) {}
-
-    public Role(Long id, String role) {
-        this.id = id;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    // Use this code if your lombok is not working:
     public String getRole() {
-        return role;
+    return role;
     }
 
     public void setRole(String role) {
-        this.role = role;
+    this.role = role;
+    }
+
+    public Long getId() {
+    return id;
     }
 
     @Override
     public String toString() {
-        return "Role [id=" + id + ", role=" + role + "]";
+    return "Role [id=" + id + ", role=" + role + "]";
     }
-    
-    
-    
+
 }
--- src/main/java/com/tts/techtalenttwitter/model/User.java
+++ src/main/java/com/tts/techtalenttwitter/model/User.java
@@ -1,6 +1,7 @@
 package com.tts.techtalenttwitter.model;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -15,14 +16,12 @@ import javax.persistence.ManyToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-
-
 @Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="user_Id")
+    @Column(name = "user_Id")
     private Long id;
 
     private String email;
@@ -38,5 +37,90 @@ public class User {
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
+
+    public CharSequence getPassword() {
+        return null;
+    }
+
+    public void setPassword(String encode) {
+    }
+
+    public void setActive(int i) {
+    }
+
+    public void setRoles(HashSet<Role> hashSet) {
+    }
+
+    public String getUsername() {
+        return null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
     
+    
+
+
+    
+    
+
+    
+    
+
+    
+    
+
 }
